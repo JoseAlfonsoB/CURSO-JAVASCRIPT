53c6577bc068542820471d6cc9aba9a2e45a1c8b
JoseAlfonsoB <jalfonsobautista74@gmail.com>
2025-07-20 00:07:03 -0600

    Update 00-hellow-word.js

--- Basic/00-hellow-word.js
+++ Basic/00-hellow-word.js
@@ -1,1 +1,17 @@
+// Este es un comentario en JavaScript de una sola linea
+
+/*  Y Este es un comentario 
+    en JavaScript
+    de varias lineas 
+*/
+
 console.log('Hola Mundo desde JavaScript!');
+
+// Podemos imprimir también operaciones desde un console.log
+
+console.log(2 + 3); // Suma
+console.log(2 - 3); // Resta
+console.log(2 * 3); // Multiplicación
+console.log(2 / 3); // División
+console.log(2 % 3); // Módulo (resto de la división) 
+console.log(2 ** 3); // Exponenciación (2 elevado a la 3)
