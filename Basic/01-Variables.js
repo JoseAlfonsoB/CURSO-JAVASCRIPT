df7b65eb28113b51a9a5dbf795bb75ed70134941
JoseAlfonsoB <jalfonsobautista74@gmail.com>
2025-07-20 14:39:56 -0600

    Se creo el archivo para el tema de  variables.

---
+++ Basic/01-Variables.js
@@ -0,0 +1,21 @@
+console.log(helloWord);
+
+var helloWord = "Hello World desde JavaScript con 'var'";
+console.log(helloWord);
+
+helloWord = "Hello World con 'var'";
+console.log(helloWord);
+
+/* 
+* Con las variables de tipo LET no podemos y JS no nos permite acceder a su valor antes de su declaración 
+*/
+// console.log(helloWord2); //! Esto JS no nos lo permite
+let helloWord2 = "Hello World desde JavaScript con 'let' 2";
+console.log(helloWord2);
+
+helloWord2 = "Hello World con 'let' 2";
+console.log(helloWord2);
+
+// console.log(helloWord3); //! Con las variables de tipo CONST tampoco podemos acceder a su valor antes de su declaración
+const helloWord3 = "Hello World desde JavaScript con 'const' 3";
+console.log(helloWord3);
